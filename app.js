005e8a6a6d1cf2ec39c3868bc2a8a521a8b619c4
Squashberrypro <squashberrypro@gmail.com>
2026-09-20 07:08:44 +0000

    Fix Netfliks installer fallback URL

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const CONFIG_URL="./config.json";
-const directFallback="https://github.com/squashberry/netfliks-downloads/releases/latest/download/Netfliks.exe";
+const directFallback="https://github.com/squashberry/netfliks-downloads/releases/latest/download/NetfliksSetup.exe";
 
 document.querySelector("#year").textContent=new Date().getFullYear();
 
